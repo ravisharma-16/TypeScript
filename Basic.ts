1e805be652392db582ae2b86530ae929ce7d5fbb
ravi sharma <raviisharmaa.2004@gmail.com>
2026-08-29 20:47:00 +0530

    Refactor Basic.ts to add console logging for special types and clarify type handling

--- Basic.ts
+++ Basic.ts
@@ -26,8 +26,64 @@ for(let idx of arr)
         break;
     }
 }
-console.log(checkVal);
+// console.log(checkVal);
 
 let ravi : String = 'ravi sharma';
 
-console.log(ravi,"hello");
+// console.log(ravi,"hello");
+
+// let check2 : 'correct' | 'notccorrect' | 'sahi_hai' | 'sahi_nahi_hai' = 'correct';
+// check2 = "notccorrect"
+
+//                                SPECIAL TYPES (any , never , unknown ,  UNDEFINED & NULL)
+
+let check = (num : Number) : void => 
+{
+    console.log(num);
+}
+
+//                          ANY
+// The any type is the most flexible type in TypeScript.
+// It essentially tells the compiler to skip type checking for a particular variable.
+// While this can be useful in certain situations, it should be used sparingly as it bypasses TypeScript's type safety features.
+let check2 = (num : any) : void => 
+{
+    console.log(num);
+}
+
+//                           NEVER
+// The never type represents the type of values that never occur.
+// It's used to indicate that something never happens or should never happen.
+type numberof = 11 | 22;
+let check3 = (num : numberof) : void => 
+{
+    console.log(num);
+}
+
+//                          UNKNOWN
+// The unknown type is a type-safe counterpart of any.
+// It's the type-safe way to say "this could be anything, so you must perform some type of checking before you use it".
+let check4 = (val : unknown) : void => 
+{
+    if(typeof val === 'string')
+    {
+        console.log(val.toUpperCase());
+    }
+    else if(typeof val === 'number')
+        {
+            console.log(typeof val);
+        }
+}
+
+// check(44);
+// check2('ravi');
+// check3(22);
+// check4(77);
+
+//                          UNDEFINED & NULL
+// In TypeScript, both undefined and null have their own types, just like string or number.
+// By default, these types can be assigned to any other type, but this can be changed with TypeScript's strict null checks.
+
+let y : undefined = undefined;
+let x : null = null;
+
